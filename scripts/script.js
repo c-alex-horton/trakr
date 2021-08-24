738b34a207fc29efb410143a9b39e72097437ea2
Caleb Horton <c.alex.horton@gmail.com>
2021-08-23 23:06:46 -0500

    finished basic  stopwatch functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,66 @@
 let timeButton = document.getElementById('time-btn');
 
+let counting = false;
+
 trackTime = () => {
     if (timeButton.innerHTML === 'Start') {
         timeButton.innerHTML = 'Stop';
+        counting = true;
+        clockFunc();
+
     } else {
         timeButton.innerHTML = 'Start';
+        counting = false;
     }
 
 }
 
+let hour = 0;
+let minute = 0;
+let second = 0;
+let currentTime = "0";
+
+clockFunc = () => {
+    if (counting === true) {
+        hour = parseInt(hour);
+        minute = parseInt(minute);
+        second = parseInt(second);
+
+        second += 1;
+
+        if (second === 60) {
+            second = 0;
+            minute += 1;
+        }
+
+        if (minute === 60) {
+            minute = 0;
+            hour += 1;
+        }
+
+        if (second < 10) {
+            second = '0' + second;
+        }
+
+        if (minute < 10) {
+            minute = '0' + minute;
+        }
+        
+        if (hour < 10) {
+            hour = '0' + hour;
+        }
+
+
+
+        currentTime = `${hour}:${minute}:${second}`;
+        document.getElementById("timer").innerHTML = currentTime;
+
+        setTimeout(clockFunc, 1000);
+    }
+}
+
+
+
 timeButton.onclick = function(){trackTime()};
+
 
